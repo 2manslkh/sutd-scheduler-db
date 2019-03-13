9ee90b466a36acdb74e8a3b47c1eba8154f6c885
jenyangg <39770775+jenyangg@users.noreply.github.com>
2019-03-13 14:57:58 +0800

    Add files via upload

--- django/mysite/schedule/models.py
+++ django/mysite/schedule/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Module(models.Model):
+    name = models.CharField(max_length=200)
+    class_type = models.CharField(max_length=200)
+    class_related = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    duration = models.CharField(max_length=200)
+    others = models.CharField(max_length=200)
+    makeup = models.CharField(max_length=200)
+    assigned_Professors = models.CharField(max_length=200)
+    course_Lead = models.CharField(max_length=200)
+    Cohort_Size = models.CharField(max_length=200)
+    Enrolment_Size = models.CharField(max_length=200)
+    
