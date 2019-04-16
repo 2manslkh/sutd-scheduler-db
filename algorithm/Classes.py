13f5c8902e8556109c576d4f3c777edfa3bd1aef
KinKi <yueqin_li@mymail.sutd.edu.sg>
2019-04-16 15:11:40 +0800

    update

--- algorithm/Classes.py
+++ algorithm/Classes.py
@@ -40,31 +40,33 @@ class Professor:
 class CourseClass:
     classes = None
 
-    def __init__(self, code, duration, pillar, isLecture = False, isLab = False):
+    def __init__(self, code, duration, pillar, isLecture = False, isLab = False, isHASS = False):
         self.code = code
         self.duration = duration
         self.pillar = pillar
         self.isLecture = isLecture
         self.isLab = isLab
+        self.isHASS = isHASS
         
     @staticmethod
-    def find(code):
+    def find(code, duration):
         for i in range(len(CourseClass.classes)):
-            if CourseClass.classes[i].code == code:
+            if CourseClass.classes[i].code == code and CourseClass.classes[i].duration == duration:
                 return i
         return -1
+
 
 
     def __repr__(self):
         if self.isLecture == True:
             #return "Course: " + str(self.code) + " lecture"
-            return str(self.code) + " lecture"
+            return str(self.code) + " lecture" + " duration " + str(self.duration)
         elif self.isLab == True:
             #return "Course: " + str(self.code) + " lab"
-            return str(self.code) + " lab"
+            return str(self.code) + " lab" + " duration " + str(self.duration)
         else:
             #return "Course: " + str(self.code) + " cohort"
-            return str(self.code) + " cohort"
+            return str(self.code) + " cohort" + " duration " + str(self.duration)
         
 class Room:
     rooms = None
@@ -91,9 +93,10 @@ class Room:
 class Slot:
     slots = None
 
-    def __init__(self, block, day):
+    def __init__(self, block, day, isHASS = False):
         self.block = block
         self.day = day
+        self.isHASS = isHASS
         
     def find(block, day):
         for i in range(len(Slot.slots)):
@@ -102,15 +105,15 @@ class Slot:
         return -1
      
     def hour_start(self):
-        start_hour_count = int((self.block[0] + 1)/2)
+        start_hour_count = int((self.block[0])/2)
         start_hour = 8 + start_hour_count
         return str(start_hour)
     
     def minute_start(self):
-        start_minute_count = self.block[-1] % 2
+        start_minute_count = (self.block[0]) % 2
         if start_minute_count == 0:
-            return "30"
-        return "00"
+            return "00"
+        return "30"
     
     def hour_end(self):
         end_hour_count = int((self.block[-1] + 1)/2)
@@ -118,7 +121,7 @@ class Slot:
         return str(end_hour)
     
     def minute_end(self):
-        end_minute_count = self.block[-1] % 2
+        end_minute_count = (self.block[-1]) % 2
         if end_minute_count == 0:
             return "30"
         return "00"
@@ -126,8 +129,7 @@ class Slot:
     def __repr__(self):
         #return "Slot: " + Slot.hour_start(self) + ":" + Slot.minute_start(self) + " - " + Slot.hour_end(self) + ":" + Slot.minute_end(self)
         #return "Slot: " + str(self.block[0]) + " - " + str(self.block[-1]) + " Day: " + str(self.day)
-        return str(self.block) + "\\" + str(self.day)  
-'''
+        return str(self.block) + "\\" + str(self.day)#  + str(self.isHASS)
+
 #test
-print(Slot([4,5,6,7], "Mon"))
-'''
+#print(Slot([1,6], "Mon"))
