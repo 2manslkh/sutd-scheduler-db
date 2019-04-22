d62cbd16355af014556e2855e1e67aff4d958379
Huan An Cheng <huanan_cheng@mymail.sutd.edu.sg>
2019-04-23 00:23:15 +0800

    updated schedulerequest model and form

--- django/mysite/formstoadmin/models.py
+++ django/mysite/formstoadmin/models.py
@@ -2,9 +2,8 @@ from django.db import models
 from multiselectfield import MultiSelectField
 
 PREFERRED_TIMINGS = (
-    ('morning', 'Morning'),
-    ('earlyAfternoon', 'Early Afternoon'),
-    ('lateAfternoon', 'Late Afternoon')
+    ('Morning', 'Morning'),
+    ('Afternoon', 'Afternoon'),
 )
 
 PILLARS = (
@@ -19,8 +18,10 @@ PILLARS = (
 
 class ScheduleRequest(models.Model):
     name = models.CharField(max_length=200)
-    course_code = models.CharField(max_length=200)
+    course_name = models.CharField(max_length=200)
     class_related = models.CharField(max_length=200)
+    duration = models.CharField(max_length=200)
+    lesson_type = models.CharField(max_length=200)
     preferred_timings = MultiSelectField(choices=PREFERRED_TIMINGS)
     reasons = models.CharField(max_length=200)
     remarks = models.CharField(max_length=200)
